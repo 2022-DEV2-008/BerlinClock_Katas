7c40b77c1e961ba85a953a875866e6dc6811b01a
Shuaib Ahmed <shuaib.ahmed@capfront.in>
2022-08-29 15:46:06 +0200

    Initial commit and dependencies added

---
+++ app/src/main/java/be/katas/berlinclock/BerlinClockActivity.kt
@@ -0,0 +1,11 @@
+package be.katas.berlinclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BerlinClockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_berlin)
+    }
+}
